a08de70a44acfc13ea9fed951b3149dbdb2ad518
Nedostup Sofia <sonined@gmail.com>
2026-03-04 15:31:22 +0300

    Add about me

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+about_me = {
+    "name": "Софья",
+    "surname": "Недоступ",
+    "email": "sonined@gmail.com"
+}
 
 
 @app.route("/")  # Это первый URL, который мы будем обрабатывать
 def hello_world():  # Эта функция-обработчик будет вызвана при запросе этого URL
     return "Hello, World!"
+
+
+@app.route("/about")
+def about():
+    return about_me
 
 
 if __name__ == "__main__":
